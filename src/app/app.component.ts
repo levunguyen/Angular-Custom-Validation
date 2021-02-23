04373a4120d8acb6427e562386a13e9d706c5d70
Trung Doan <duytrung177@gmail.com>
2021-02-23 11:52:32 +0700

    up project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import {FormControl, FormGroup} from '@angular/forms';
+import {gte} from './gte.validator';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'custom-validate';
+  myForm = new FormGroup({
+    numVal: new FormControl('', [gte])
+  });
+
+  get numVal(){
+    return this.myForm.get('numVal');
+  }
+  onSubmit() {
+    console.log(this.myForm.value);
+  }
 }
